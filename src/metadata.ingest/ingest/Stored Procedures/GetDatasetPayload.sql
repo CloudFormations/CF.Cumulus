778c27c28058df16291cecca5ab8b73c3fa7e01c
MattPCollins <mc12338@my.bristol.ac.uk>
2024-07-22 17:10:11 +0100

    Update ingest raiserror condition bug

--- src/metadata.ingest/ingest/Stored Procedures/GetDatasetPayload.sql
+++ src/metadata.ingest/ingest/Stored Procedures/GetDatasetPayload.sql
@@ -24,6 +24,7 @@ BEGIN
     IF @ResultRowCount = 0
     BEGIN
         RAISERROR('No results returned for the provided Dataset Id. Confirm Dataset is enabled, and related Connections are enabled.',16,1)
+		RETURN 0;
     END
     
 
@@ -109,8 +110,10 @@ BEGIN
             ds.DatasetId = @DatasetId
     END
 	ELSE
+	BEGIN
 		RAISERROR('Language Type not supported.',16,1)
-
+		RETURN 0;
+	END
     -- Update with new logic
     IF (@LoadType = 'F')
 		BEGIN
@@ -132,6 +135,7 @@ BEGIN
 	ELSE
 		BEGIN
 			RAISERROR('Load type condition not yet supported.',16,1);
+			RETURN 0;
 		END
 	
 	IF @SourceLanguageType IN ('T-SQL', 'PSQL', 'SQL')
