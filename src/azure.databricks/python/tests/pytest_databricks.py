33185361ee753249f8053e8c477f7199c383fe23
MattPCollins <MattPCollins>
2025-05-27 07:56:15 +0000

    Update pytest runner for correct paths

--- src/azure.databricks/python/tests/pytest_databricks.py
+++ src/azure.databricks/python/tests/pytest_databricks.py
@@ -3,7 +3,7 @@
 
 # COMMAND ----------
 
-!cp ../requirements.txt ~/.
+!cp ../../../../requirements.txt ~/.
 %pip install -r ~/requirements.txt
 
 # COMMAND ----------
@@ -25,7 +25,7 @@ import sys
 # Run all tests in the repository root.
 notebook_path = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
 repo_root = os.path.dirname(os.path.dirname(notebook_path))
-os.chdir(f'/Workspace/Live2/{repo_root}')
+os.chdir(f'/Workspace/{repo_root}')
 %pwd
 
 # Skip writing pyc files on a readonly filesystem.
@@ -35,3 +35,7 @@ retcode = pytest.main([".", "-p", "no:cacheprovider"])
 
 # Fail the cell execution if we have any test failures.
 assert retcode == 0, 'The pytest invocation failed. See the log above for details.'
+
+# COMMAND ----------
+
+
