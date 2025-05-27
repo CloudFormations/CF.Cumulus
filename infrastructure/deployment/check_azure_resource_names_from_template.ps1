591e6791c90c393ad8eaf29893f2f419f22fe92a
CFToby-Andrew <toby.andrew@cloudformations.org>
2025-05-27 14:04:00 +0100

    Removed unused PS scripts

--- infrastructure/deployment/check_azure_resource_names_from_template.ps1
+++
@@ -1,8 +0,0 @@
-#Assigining the parameters for the environment
-param(
-    [Parameter(Mandatory=$true)]
-    [string] $parametersFile
-)
-
-Get-Content -Path infrastructure\configuration\_installation\main.bicepparam
-
