e9ef409c514ef25a0e2894d1418a4e28976c2aab
CFToby-Andrew <toby.andrew@cloudformations.org>
2025-06-10 14:54:54 +0100

    create yml files for core, data and master

--- src/metadata.data/Scripts/ControlPipelineMetadataAsCode.sql
+++ src/metadata.data/Scripts/ControlPipelineMetadataAsCode.sql
@@ -7,15 +7,8 @@ EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Raw', @PipelineN
 EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Cleansed', @PipelineName='Ingest_PL_Merge', @DatasetDisplayName='SalesOrderHeader', @OrchestratorName='$(ADFName)', @ComponentName = 'Ingest';
 EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Cleansed', @PipelineName='Ingest_PL_Merge', @DatasetDisplayName='SalesOrderDetail', @OrchestratorName='$(ADFName)', @ComponentName = 'Ingest';
 EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Cleansed', @PipelineName='Ingest_PL_Merge', @DatasetDisplayName='Product', @OrchestratorName='$(ADFName)', @ComponentName = 'Ingest';
-EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Curated', @PipelineName='Transform_PL_Unmanaged', @DatasetDisplayName='DimDate', @OrchestratorName='$(ADFName)', @ComponentName = 'Transform';
-EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Curated', @PipelineName='Transform_PL_Unmanaged', @DatasetDisplayName='DimProducts', @OrchestratorName='$(ADFName)', @ComponentName = 'Transform';
-EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Serve', @PipelineName='Transform_PL_Unmanaged', @DatasetDisplayName='FactSales', @OrchestratorName='$(ADFName)', @ComponentName = 'Transform';
+EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Dimension', @PipelineName='Transform_PL_Unmanaged', @DatasetDisplayName='DimDate', @OrchestratorName='$(ADFName)', @ComponentName = 'Transform';
+EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Dimension', @PipelineName='Transform_PL_Unmanaged', @DatasetDisplayName='DimProducts', @OrchestratorName='$(ADFName)', @ComponentName = 'Transform';
+EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Fact', @PipelineName='Transform_PL_Unmanaged', @DatasetDisplayName='FactSales', @OrchestratorName='$(ADFName)', @ComponentName = 'Transform';
 
 --Pipeline Dependencies
-EXEC [ingest].[AddIngestPayloadPipelineDependencies] @DatasetDisplayName='SalesOrderHeader';
-EXEC [ingest].[AddIngestPayloadPipelineDependencies] @DatasetDisplayName='SalesOrderDetail';
-EXEC [ingest].[AddIngestPayloadPipelineDependencies] @DatasetDisplayName='Product';
-
-EXEC [transform].[AddTransformPayloadPipelineDependencies] @DatasetDisplayName='DimDate';
-EXEC [transform].[AddTransformPayloadPipelineDependencies] @DatasetDisplayName='DimProducts';
-EXEC [transform].[AddTransformPayloadPipelineDependencies] @DatasetDisplayName='FactSales';
