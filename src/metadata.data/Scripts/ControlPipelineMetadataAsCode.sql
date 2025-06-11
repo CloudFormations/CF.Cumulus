30a20549fae30d259280ae1c149309a3149ee6a6
MattPCollins <mc12338@my.bristol.ac.uk>
2025-06-11 17:08:29 +0100

    Pipeline name tidy-up

--- src/metadata.data/Scripts/ControlPipelineMetadataAsCode.sql
+++ src/metadata.data/Scripts/ControlPipelineMetadataAsCode.sql
@@ -7,9 +7,9 @@ EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Raw', @PipelineN
 EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Cleansed', @PipelineName='Ingest_PL_Merge', @DatasetDisplayName='SalesOrderHeader', @OrchestratorName='$(ADFName)', @ComponentName = 'Ingest';
 EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Cleansed', @PipelineName='Ingest_PL_Merge', @DatasetDisplayName='SalesOrderDetail', @OrchestratorName='$(ADFName)', @ComponentName = 'Ingest';
 EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Cleansed', @PipelineName='Ingest_PL_Merge', @DatasetDisplayName='Product', @OrchestratorName='$(ADFName)', @ComponentName = 'Ingest';
-EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Dimension', @PipelineName='Transform_PL_managed', @DatasetDisplayName='DimDate', @OrchestratorName='$(ADFName)', @ComponentName = 'Transform';
-EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Dimension', @PipelineName='Transform_PL_managed', @DatasetDisplayName='DimProducts', @OrchestratorName='$(ADFName)', @ComponentName = 'Transform';
-EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Fact', @PipelineName='Transform_PL_managed', @DatasetDisplayName='FactSales', @OrchestratorName='$(ADFName)', @ComponentName = 'Transform';
+EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Dimension', @PipelineName='Transform_PL_Managed', @DatasetDisplayName='DimDate', @OrchestratorName='$(ADFName)', @ComponentName = 'Transform';
+EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Dimension', @PipelineName='Transform_PL_Managed', @DatasetDisplayName='DimProducts', @OrchestratorName='$(ADFName)', @ComponentName = 'Transform';
+EXEC [common].[AddIngestOrTransformPayloadPipeline] @StageName='Fact', @PipelineName='Transform_PL_Managed', @DatasetDisplayName='FactSales', @OrchestratorName='$(ADFName)', @ComponentName = 'Transform';
 
 --Pipeline Dependencies
 EXEC [common].[AddGenericPayloadPipelineDependencies] @DatasetDisplayName='SalesOrderHeader', @StageName='Raw', @DependantDatasetDisplayName='SalesOrderHeader', @DependantStageName='Cleansed';
