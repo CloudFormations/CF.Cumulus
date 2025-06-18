4cbd25b632e569e2f7cc7279d3353bc3a4add8ce
MattPCollins <mc12338@my.bristol.ac.uk>
2025-06-18 09:12:25 +0100

    Uncommenting

--- infrastructure/deployment/deploy_sql_dacpacs.ps1
+++ infrastructure/deployment/deploy_sql_dacpacs.ps1
@@ -46,17 +46,17 @@ $sqlPassword = az keyvault secret show --name $sqlValueSecret --vault-name $keyV
 $currentLocation = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
 $sourceFolderPath = $currentLocation -replace '\\infrastructure\\deployment'
 
-# # Publish the common schema DacPac
-# SqlPackage /Action:Publish /SourceFile:"$sourceFolderPath\src\metadata.common\metadata.common.dacpac" /TargetConnectionString:"Server=tcp:$sqlServerName.database.windows.net,1433;Initial Catalog=$sqlDatabaseName;Persist Security Info=False;User ID=$sqlLogin;Password=$sqlPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" /v:DatabricksWSName=$databricksWorkspaceName /v:DatabricksHost="https://$databricksWorkspaceURL" /v:DLSName=$storageAccountName  /v:Environment="Dev"  /v:KeyVaultName=$keyVaultName  /v:RGName=$resourceGroupName /v:SubscriptionID=$subscriptionIdValue 
+# Publish the common schema DacPac
+SqlPackage /Action:Publish /SourceFile:"$sourceFolderPath\src\metadata.common\metadata.common.dacpac" /TargetConnectionString:"Server=tcp:$sqlServerName.database.windows.net,1433;Initial Catalog=$sqlDatabaseName;Persist Security Info=False;User ID=$sqlLogin;Password=$sqlPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" /v:DatabricksWSName=$databricksWorkspaceName /v:DatabricksHost="https://$databricksWorkspaceURL" /v:DLSName=$storageAccountName  /v:Environment="Dev"  /v:KeyVaultName=$keyVaultName  /v:RGName=$resourceGroupName /v:SubscriptionID=$subscriptionIdValue 
 
-# # Publish the control schema DacPac
-# SqlPackage /Action:Publish /SourceFile:"$sourceFolderPath\src\metadata.control\metadata.control.dacpac" /TargetConnectionString:"Server=tcp:$sqlServerName.database.windows.net,1433;Initial Catalog=$sqlDatabaseName;Persist Security Info=False;User ID=$sqlLogin;Password=$sqlPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" /v:Environment="Dev"  /v:RGName=$resourceGroupName /v:SubscriptionID=$subscriptionIdValue /v:ADFName=$dataFactoryName /v:TenantID=$tenantId
+# Publish the control schema DacPac
+SqlPackage /Action:Publish /SourceFile:"$sourceFolderPath\src\metadata.control\metadata.control.dacpac" /TargetConnectionString:"Server=tcp:$sqlServerName.database.windows.net,1433;Initial Catalog=$sqlDatabaseName;Persist Security Info=False;User ID=$sqlLogin;Password=$sqlPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" /v:Environment="Dev"  /v:RGName=$resourceGroupName /v:SubscriptionID=$subscriptionIdValue /v:ADFName=$dataFactoryName /v:TenantID=$tenantId
 
-# # Publish the ingest schema DacPac
-# SqlPackage /Action:Publish /SourceFile:"$sourceFolderPath\src\metadata.ingest\metadata.ingest.dacpac" /TargetConnectionString:"Server=tcp:$sqlServerName.database.windows.net,1433;Initial Catalog=$sqlDatabaseName;Persist Security Info=False;User ID=$sqlLogin;Password=$sqlPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" 
+# Publish the ingest schema DacPac
+SqlPackage /Action:Publish /SourceFile:"$sourceFolderPath\src\metadata.ingest\metadata.ingest.dacpac" /TargetConnectionString:"Server=tcp:$sqlServerName.database.windows.net,1433;Initial Catalog=$sqlDatabaseName;Persist Security Info=False;User ID=$sqlLogin;Password=$sqlPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" 
 
-# # Publish the transform schema DacPac
-# SqlPackage /Action:Publish /SourceFile:"$sourceFolderPath\src\metadata.transform\metadata.transform.dacpac" /TargetConnectionString:"Server=tcp:$sqlServerName.database.windows.net,1433;Initial Catalog=$sqlDatabaseName;Persist Security Info=False;User ID=$sqlLogin;Password=$sqlPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" 
+# Publish the transform schema DacPac
+SqlPackage /Action:Publish /SourceFile:"$sourceFolderPath\src\metadata.transform\metadata.transform.dacpac" /TargetConnectionString:"Server=tcp:$sqlServerName.database.windows.net,1433;Initial Catalog=$sqlDatabaseName;Persist Security Info=False;User ID=$sqlLogin;Password=$sqlPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" 
 
 
 # Set Entra AD admin to myself for this:
