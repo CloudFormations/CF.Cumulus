2dbb2283948935f4daa24683765e5ae9a97abfc3
Paul Andrew <60190428+mrpaulandrew@users.noreply.github.com>
2026-04-27 11:04:49 +0100

    Azure Function App Alerting Overhaul (#199)
    * Overhaul of alerting services via Azure Functions
    
    * Deleting pipeline: Control_PL_BatchExecutor
    
    * Adding pipeline: Control_PL_BatchExecutor
    
    * Updating pipeline: Utils_PL_Email_Sender
    
    * Updating pipeline: Control_PL_PipelineExecutor
    
    * Updating pipeline: Control_PL_PipelineExecutor
    
    * Updating pipeline: Utils_PL_Email_Sender

---
+++ src/azure.functionapp/services/alerts/SendGridAlertService.cs
@@ -0,0 +1,27 @@
+﻿using cloudformations.cumulus.helpers;
+using cloudformations.cumulus.returns;
+using Microsoft.Extensions.Logging;
+
+namespace cloudformations.cumulus.services
+{
+    internal class SendGridAlertService : AlertService
+    {
+        private PipelineAlertRequest ar;
+        private ILogger logger;
+
+        public SendGridAlertService(PipelineAlertRequest ar, ILogger logger)
+        {
+            this.ar = ar;
+            this.logger = logger;
+        }
+
+        public override PipelineAlertDetail PipelineSendAlert(PipelineAlertRequest request)
+        {
+            throw new NotImplementedException();
+        }
+        public override void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
